187fa914f7e281072fdbcb630c640d68a5933f2a
cankarovski <gcankar@gmail.com>
2021-04-05 14:34:43 +0200

    API_URL repeating code fixed

--- todo-app/src/api/api.js
+++ todo-app/src/api/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "http://localhost:8000/api/todos";
 
 const api = {
   getTodos: async () => {
-    let res = await axios.get(`${API_URL}/todos`);
+    let res = await axios.get(`${API_URL}`);
     console.log(res);
     return res.data;
   },
   addTodo: async (todo) => {
-    let res = await axios.post(`${API_URL}/todos`, todo);
+    let res = await axios.post(`${API_URL}`, todo);
     console.log(res);
     return res;
   },
   editTodo: async (todo) => {
-    let res = await axios.put(`${API_URL}/todos/${todo._id}`, todo);
+    let res = await axios.put(`${API_URL}/${todo._id}`, todo);
     console.log(res);
     return res;
   },
   deleteTodo: async (todo) => {
-    let res = await axios.delete(`${API_URL}/todos/${todo._id}`, todo);
+    let res = await axios.delete(`${API_URL}/${todo._id}`, todo);
     console.log(res);
     return res.status;
   },
